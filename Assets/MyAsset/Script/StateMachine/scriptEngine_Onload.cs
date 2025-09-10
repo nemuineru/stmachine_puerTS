a3ba8a3ea76572a51f9244b0a3e6750c65ff8860
nemuineru <suteado16x16@gmail.com>
2025-09-10 22:53:55 +0900

    seein evals. its nice to see those JS/TS works.

--- Assets/MyAsset/Script/StateMachine/scriptEngine_Onload.cs
+++ Assets/MyAsset/Script/StateMachine/scriptEngine_Onload.cs
@@ -22,15 +22,37 @@ public class scriptEngine_Onload : MonoBehaviour
     }
     void Start()
     {
-        scEnv.Eval(@"
-        console.log('hello world');
-        ");
     }
+
+    float x = 0;
+    float repTime = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        x += Time.deltaTime;
+        bool switcher = repTime / 2.0f >= Mathf.Repeat(x, repTime);
+        if (switcher)
+        {
+                textMesh.text = scEnv.Eval<string>(@"
+            (function()
+            {
+                console.log('hello world');
+                return 'Hello_World';
+            })()
+            ");
+        }
+        else
+        { 
+            
+            textMesh.text = scEnv.Eval<string>(@"
+                (function()
+                {
+                    console.log('goodbye world');
+                    return 'GoodBye_World';
+                })()
+                ");
+        }
     }
 
     void OnDestroy()
