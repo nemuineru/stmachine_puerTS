a53da6c3a256f5020bace16fbbc3d879eb03ef37
nemuineru <suteado16x16@gmail.com>
2025-09-10 23:18:04 +0900

    seek the debugs

--- Assets/MyAsset/Script/StateMachine/scriptEngine_Onload.cs
+++ Assets/MyAsset/Script/StateMachine/scriptEngine_Onload.cs
@@ -26,37 +26,44 @@ public class scriptEngine_Onload : MonoBehaviour
 
     float x = 0;
     float repTime = 1.0f;
+    bool isInDebug = false;
 
     // Update is called once per frame
     void Update()
     {
-        x += Time.deltaTime;
-        bool switcher = repTime / 2.0f >= Mathf.Repeat(x, repTime);
-        if (switcher)
+        if (isInDebug)
         {
-                textMesh.text = scEnv.Eval<string>(@"
+            x += Time.deltaTime;
+            bool switcher = repTime / 2.0f >= Mathf.Repeat(x, repTime);
+            if (textMesh != null)
+            {
+                if (switcher)
+                {
+                    textMesh.text = scEnv.Eval<string>(@"
             (function()
             {
                 console.log('hello world');
                 return 'Hello_World';
             })()
             ");
-        }
-        else
-        { 
-            
-            textMesh.text = scEnv.Eval<string>(@"
+                }
+                else
+                {
+
+                    textMesh.text = scEnv.Eval<string>(@"
                 (function()
                 {
                     console.log('goodbye world');
                     return 'GoodBye_World';
                 })()
                 ");
+                }
+            }
         }
     }
 
     void OnDestroy()
     {
-        //LEnv.Dispose();
+        scEnv.Dispose();
     }
 }
